d1b415fcdf90f6f93c0ac2fa1a70f2e44c0e6c9c
spcwhl <mike@spcwhl.com>
2015-06-20 18:00:43 -0700

    Forgot 8

---
+++ hashes/exercise8.rb
@@ -0,0 +1,5 @@
+# if you see this error, what do you suspect is the most likely problem?
+
+# NoMethodError: undefined method 'keys' for Array 
+
+B. There is no method called keys for Array objects.
